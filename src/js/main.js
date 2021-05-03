a56869a63a080f64895cb1947a6f1afb7bef8962
andreysey <andreyseynew@gmail.com>
2021-05-03 17:15:23 +0300

    add scroll

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@ import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordion from "./modules/accordion";
+import burger from "./modules/burger";
+import scrolling from "./modules/scrolling";
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -27,4 +29,6 @@ window.addEventListener('DOMContentLoaded', () => {
     filter();
     pictureSize('.sizes-block');
     accordion('.accordion-heading');
+    burger('.burger-menu', '.burger');
+    scrolling('.pageup');
 });
